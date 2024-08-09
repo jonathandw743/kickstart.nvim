10c3ac378fe41382cfee6c44f45b9ec1eacfa7ab
Jonathan Wilson <jonathandw414@gmail.com>
2024-08-09 18:26:59 +0100

    cmd without shift (;)

--- init.lua
+++ init.lua
@@ -181,6 +181,8 @@ vim.keymap.set('n', '<leader>tr', '<cmd>set relativenumber!<CR>', { desc = '[T]o
 
 vim.keymap.set('n', '<C-s>', '<cmd>w<CR>', { desc = 'Save' })
 
+vim.keymap.set({ 'n', 'v' }, ';', ':')
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
