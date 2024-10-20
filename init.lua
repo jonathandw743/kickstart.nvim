2d78a513e011210d5c26c96993c394257a53a907
jonathandw743 <jonathandw414@gmail.com>
2024-10-20 22:41:38 +0100

    comments

--- init.lua
+++ init.lua
@@ -671,7 +671,9 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        -- sorts python imports
         'isort',
+        -- opinionated python formatter
         'black',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
