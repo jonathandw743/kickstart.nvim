80e6006c6c018f777a889d31297baf6370d591cd
jonathandw743 <jonathandw414@gmail.com>
2025-07-17 19:21:59 +0100

    black isort

--- init.lua
+++ init.lua
@@ -754,6 +754,8 @@ require('lazy').setup({
         -- ts_ls = {},
         --
         wgsl_analyzer = {},
+        black = {},
+        isort = {},
 
         lua_ls = {
           -- cmd = { ... },
@@ -841,7 +843,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
